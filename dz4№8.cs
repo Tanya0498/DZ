92db30787d196424d251cb896c7cb4b1772591b2
Tanya0498 <112667444+Tanya0498@users.noreply.github.com>
2022-09-02 03:45:49 +0300

    Add files via upload

---
+++ dz4№8.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int numberN = int.Parse (Console.ReadLine());
+int numberX = 1;
+while (numberX <= numberN)
+{
+    if (numberX%2 == 0) 
+    {
+        Console.WriteLine(numberX);
+    }
+    numberX = numberX + 1;
+}
